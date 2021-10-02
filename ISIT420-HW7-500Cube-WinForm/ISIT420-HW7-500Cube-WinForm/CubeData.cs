23f7c878c18bb1a135fddc86ba4c9c4a2fd4f0eb
Darren Ross <darren.ross@bellevuecollege.edu>
2021-10-02 02:15:08 -0700

    Completed assignment.

---
+++ ISIT420-HW7-500Cube-WinForm/ISIT420-HW7-500Cube-WinForm/CubeData.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AnalysisServices.AdomdClient;
+
+namespace ISIT420_HW7_500Cube_WinForm
+{
+    class CubeData
+    {
+        public DataSet GetQueryOne()
+        {
+            string query = "SELECT {[Measures].[Orders Count]} ON COLUMNS, " +
+                            "NON EMPTY ORDER(([Sales Person Table].[First Name].CHILDREN, [CD Table].[C Dname].CHILDREN, [CD Table].[List Price].CHILDREN), [Measures].[Orders Count], BDESC) ON ROWS " +
+                            "FROM[Node Orders500T];";
+
+            DataSet cubeData = new DataSet();
+
+            AdomdDataAdapter myDataAdapter = new AdomdDataAdapter();
+
+            string connString = @"Data Source=DAGDA;Catalog=ISIT420-HW7-500Cube";
+            AdomdConnection conn = new AdomdConnection(connString);
+
+            myDataAdapter.SelectCommand = new AdomdCommand(query, conn);
+
+            try
+            {
+                myDataAdapter.Fill(cubeData, "CubeData1");
+            }
+            catch(Exception e)
+            {
+                throw new ApplicationException(e.Message);
+            }
+            return cubeData;
+        }
+
+        public DataSet GetQueryTwo()
+        {
+            string query = "SELECT NON EMPTY([CD Table].[List Price].CHILDREN, [Measures].[Orders Count]) ON COLUMNS, " +
+                           "NON EMPTY ORDER(([Sales Person Table].[First Name].CHILDREN), [Measures].[Orders Count], BDESC) ON ROWS " +
+                           "FROM[Node Orders500T];";
+
+            DataSet cubeData = new DataSet();
+
+            AdomdDataAdapter myDataAdapter = new AdomdDataAdapter();
+
+            string connString = @"Data Source=DAGDA;Catalog=ISIT420-HW7-500Cube";
+            AdomdConnection conn = new AdomdConnection(connString);
+
+            myDataAdapter.SelectCommand = new AdomdCommand(query, conn);
+
+            try
+            {
+                myDataAdapter.Fill(cubeData, "CubeData2");
+            }
+            catch (Exception e)
+            {
+                throw new ApplicationException(e.Message);
+            }
+            return cubeData;
+        }
+
+        public DataSet GetQueryThree()
+        {
+            string query = "SELECT {[Measures].[Orders Count]} ON COLUMNS, " +
+                           "NON EMPTY ORDER(([Sales Person Table - Store].[City].CHILDREN AS City, [Sales Person Table - Store].[Number Employees].CHILDREN AS NumberOfEmployees) , [Measures].[Orders Count], BDESC) ON ROWS " +
+                           "FROM[Node Orders500T];";
+
+            DataSet cubeData = new DataSet();
+
+            AdomdDataAdapter myDataAdapter = new AdomdDataAdapter();
+
+            string connString = @"Data Source=DAGDA;Catalog=ISIT420-HW7-500Cube";
+            AdomdConnection conn = new AdomdConnection(connString);
+
+            myDataAdapter.SelectCommand = new AdomdCommand(query, conn);
+
+            try
+            {
+                myDataAdapter.Fill(cubeData, "CubeData3");
+            }
+            catch (Exception e)
+            {
+                throw new ApplicationException(e.Message);
+            }
+            return cubeData;
+        }
+    }
+}
---
+++ ISIT420-HW7-500Cube-WinForm/ISIT420-HW7-500Cube-WinForm/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISIT420_HW7_500Cube_WinForm
+{
+    public partial class Form1 : Form
+    {
+        DataSet formDataSet = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CubeData myCube = new CubeData();
+            try
+            {
+                formDataSet = myCube.GetQueryOne();
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Something went wrong! " + ex.Message;
+            }
+            label1.Text = "CD sales count by Album and Sales Person ordered by price point descending.";
+            dataGridView1.DataSource = formDataSet;
+            dataGridView1.DataMember = "CubeData1";
+            dataGridView1.AutoResizeColumns();
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.Azure;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CubeData myCube = new CubeData();
+            try
+            {
+                formDataSet = myCube.GetQueryTwo();
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Something went wrong! " + ex.Message;
+            }
+            label1.Text = "Sales Person sales count by price point.";
+            dataGridView1.DataSource = formDataSet;
+            dataGridView1.DataMember = "CubeData2";
+            dataGridView1.AutoResizeColumns();
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.Azure;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CubeData myCube = new CubeData();
+            try
+            {
+                formDataSet = myCube.GetQueryThree();
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Something went wrong! " + ex.Message;
+            }
+            label1.Text = "Employee count and total sale orders by store.";
+            dataGridView1.DataSource = formDataSet;
+            dataGridView1.DataMember = "CubeData3";
+            dataGridView1.AutoResizeColumns();
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.Azure;
+        }
+    }
+}
